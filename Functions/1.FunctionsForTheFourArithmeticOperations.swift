5a2355076285d7dcc12b9237f7b7d531f6e8149d
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-08 00:16:01 -0400

    Add functions module

---
+++ Functions/1.FunctionsForTheFourArithmeticOperations.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+/**
+ # 1. Funções para as 4 operações aritméticas
+
+ Escreva uma função para cada uma das quatro operações matemáticas simples.
+
+ - adição
+ - subtração
+ - multiplicação
+ - divisão
+ 
+ */
+public func functionsForTheFourArithmeticOperations(operation: ArithmeticOperation, number1: Int, number2: Int) -> Int {
+    switch operation {
+    case .addition: return addition(number1: number1, number2: number2)
+    case .subtraction: return subtraction(number1: number1, number2: number2)
+    case .multiplication: return multiplication(number1: number1, number2: number2)
+    case .division: return division(number1: number1, number2: number2)
+    }
+}
+
+private func addition(number1: Int, number2: Int) -> Int {
+    var result = 0
+    
+    return result
+}
+
+private func subtraction(number1: Int, number2: Int) -> Int {
+    var result = 0
+    
+    return result
+}
+
+private func multiplication(number1: Int, number2: Int) -> Int {
+    var result = 0
+    
+    return result
+}
+
+private func division(number1: Int, number2: Int) -> Int {
+    var result = 0
+    
+    return result
+}
+
+public enum ArithmeticOperation {
+    case addition
+    case subtraction
+    case multiplication
+    case division
+}
---
+++ Functions/README.md
@@ -0,0 +1,15 @@
+# Funções - Lista de Exercícios Nível 1
+
+Vai exigir um pouco mais de atenção.
+
+## 1. Funções para as 4 operações aritméticas 
+
+Escreva uma função para cada uma das quatro operações matemáticas simples.
+
+- adição
+- subtração
+- multiplicação
+- divisão
+
+
+
