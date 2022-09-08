8b86e34db9b49ff5f84e78143da531f47084580f
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-08 01:12:24 -0400

    Add last exercise of function list

---
+++ Functions/4.ReadjustingTheSalary.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/**
+ # Reajustando o salário
+
+ Faça um algoritmo que calcule o reajuste de um salário, utilize os seguintes dados:
+
+ ```
+ salário  = 1.000,00
+ reajuste = 15%,
+ ```
+ */
+public func readjustingTheSalary(salary: Double, reajust: Double) -> Double {
+    return calculateReadjustTheSalary(salary: salary, reajust: reajust)
+}
+
+private func calculateReadjustTheSalary(salary: Double, reajust: Double) -> Double {
+    var result: Double = 0
+    
+    return result
+}
